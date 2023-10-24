d691e28c629d0482f4b9a4c7b6a4ec36b3349669
Amin Razbani <48209586+aminraz@users.noreply.github.com>
2023-10-24 12:23:36 +0200

    Update the value of S0 ( initial surface area)

--- micp_test_1B.py
+++ micp_test_1B.py
@@ -51,7 +51,7 @@ Calcite
 21 V_cell = 83.6 # ml
 22 d_calcite = 36.9 # cm³/mol
 23 pow_a = 2
-25 A0 = 0.5# m2/l
+25 A0 = 1.0# m2/l
 30 si_cc = SI("Calcite")
 70 Area = A0 * (initial_por/get_por(cell_no))^pow_a * soln_vol
 80 rate = Area * (parm(1) + parm(2)*act("H+")) * (1-10^SI("Calcite"))
